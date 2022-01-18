7cd62ad19b7dc47170f418da127f1b71b47acbb1
smusulin <smusulin@optum.com>
2022-01-18 00:26:19 -0700

    clean up and go back to a regular button

--- app/javascript/components/table.js
+++ app/javascript/components/table.js
@@ -6,61 +6,16 @@ import Hand from './hand'
 import HandHistory from './handHistory'
 
 const Wrapper = styled.div`
-width: 100%;
-display: flex;
-flex-direction: column;
-    `;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`;
 
-const Button = styled.div`
-position:relative;
- display:inline-block;
- margin:20px;
- a{
-  color:white;
-  font-family:Helvetica, sans-serif;
-  font-weight:bold;
-  font-size:36px;
-  text-align: center;
-  text-decoration:none;
-  background-color:#FFA12B;
-  display:block;
-  position:relative;
-  padding:20px 40px;
-  
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-  text-shadow: 0px 1px 0px #000;
-  filter: dropshadow(color=#000, offx=0px, offy=1px);
-  
-  -webkit-box-shadow:inset 0 1px 0 #FFE5C4, 0 10px 0 #915100;
-  -moz-box-shadow:inset 0 1px 0 #FFE5C4, 0 10px 0 #915100;
-  box-shadow:inset 0 1px 0 #FFE5C4, 0 10px 0 #915100;
-  
-  -webkit-border-radius: 5px;
-  -moz-border-radius: 5px;
-  border-radius: 5px;
-}
-a:active{
-  top:10px;
-  background-color:#F78900;
-  
-  -webkit-box-shadow:inset 0 1px 0 #FFE5C4, inset 0 -3px 0 #915100;
-  -moz-box-shadow:inset 0 1px 0 #FFE5C4, inset 0 -3pxpx 0 #915100;
-  box-shadow:inset 0 1px 0 #FFE5C4, inset 0 -3px 0 #915100;
- }
-:after{
-  content:"";
-  height:100%;
-  width:100%;
-  padding:4px;
-  position: absolute;
-  bottom:-15px;
-  left:-4px;
-  z-index:-1;
-  background-color:#2B1800;
-  -webkit-border-radius: 5px;
-  -moz-border-radius: 5px;
-  border-radius: 5px;
-}
+const Button = styled.button`
+    margin-top: 1rem;
+    width: 20%;
+    border-radius: 2rem;
+    height: 3rem;
 `
 
 const Board = styled.div`
@@ -72,13 +27,13 @@ const Board = styled.div`
     margin-bottom: 1rem;
 `
 const HandsWrapper = styled.div`
-min-height: 18rem;
+  min-height: 18rem;
 `
 const Hands = styled.div `
-margin: 2rem;
+  margin: 2rem;
 `
 const Winner = styled.div`
-margin:1rem;
+  margin:1rem;
 `
 
 
@@ -86,7 +41,6 @@ const Table = () => {
     const [deal, setDeal] = useState([]);
 
     const onClick = (e) => {
-        e.preventDefault();
         axios.get('/api/deal/')
             .then((resp) => {
                 setDeal(resp.data.data)
@@ -96,8 +50,10 @@ const Table = () => {
     return (
         <Wrapper>
             <Board>
-                <Button>
-                    <a href="#" onClick={onClick}>Deal</a>
+                <Button onClick={onClick}>
+                  <span >
+                    Deal
+                  </span>
                 </Button>
                 <HandsWrapper>
                     {deal.id &&
