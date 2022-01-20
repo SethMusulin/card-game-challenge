231ec3b565d4e136262f950489d7cf550131907a
smusulin <smusulin@optum.com>
2022-01-20 11:56:57 -0700

    clean up the database and reseed for Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Deal.delete_all
 
-if Deal.all.empty?
-  File.foreach("lib/seeds/poker.txt").with_index do |line, line_num|
-    begin
-      deal = Deal.create(player_1:line[0,14].chomp, player_2:line[15,29].chomp)
+File.foreach("lib/seeds/poker.txt").with_index do |line, line_num|
+  begin
+    deal = Deal.create(player_1: line[0, 14].chomp, player_2: line[15, 29].chomp)
 
-      deal.determine_winner
-      pp"creating hand #{line_num}"
-    rescue
-      pp "Error Creating line number #{line_num}"
-    end
+    deal.determine_winner
+    pp "creating hand #{line_num}"
+  rescue
+    pp "Error Creating line number #{line_num}"
   end
 end
